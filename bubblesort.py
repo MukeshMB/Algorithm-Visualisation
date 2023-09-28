9d7b805e960f295d19a87bfd4feff5ab1fd453d4
MUKESH BHARTI <94177051+MukeshMB@users.noreply.github.com>
2023-09-29 00:40:58 +0530

    bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,28 +1,47 @@
-from tempfile import tempdir
 import pygame
 
-HEIGHT = 1200
-WIDTH = 800
-WIN = pygame.display.set_mode((HEIGHT, WIDTH))
+# FRAME CONSTRUCTION
+def init_display():
+	WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+	WIDTH = 9 * pygame.display.Info().current_w // 10
+	HEIGHT = 9 * pygame.display.Info().current_h // 10
+	pygame.quit()
+	WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.display.set_caption('Bubblesort')
 
+	return WIN, WIDTH, HEIGHT
+
+# INIT FRAME
+WIN, WIDTH, HEIGHT = init_display()
+
+# INIT @params
+width = 5
+N = WIDTH // width
+
+# INIT Colours
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+ORANGE = (255, 165, 0)
+TURQUOISE = (64, 224, 208)
+PURPLE = (128, 0, 128)
+YELLOW = (102, 102, 0)
+MAGENTA = (102, 102, 105)
+
 
 class block:
-    def __init__(self, row, y, width, color):
+    def __init__(self, row, y, color):
         self.x = row * width
         self.y = y
         self.color = color
-        self.width = width
     
     def draw(self):
-        pygame.draw.rect(WIN, self.color, (self.x, self.y, self.width, WIDTH - self.y))
-        pygame.draw.rect(WIN, BLACK, (self.x, 0, self.width, self.y))
-        pygame.draw.line(WIN, BLACK, (self.x + self.width, 0), (self.x + self.width, WIDTH))
-        pygame.draw.line(WIN, BLACK, (self.x, 0), (self.x, WIDTH))
+        pygame.draw.rect(WIN, self.color, (self.x, self.y, width, HEIGHT - self.y))
+        pygame.draw.rect(WIN, BLACK, (self.x, 0, width, self.y))
+        pygame.draw.line(WIN, BLACK, (self.x + width, 0), (self.x + width, HEIGHT))
+        pygame.draw.line(WIN, BLACK, (self.x, 0), (self.x, HEIGHT))
         pygame.display.update()
         
         
@@ -35,7 +54,7 @@ def draw_all(Grid):
 def bubblesort(Grid):
     for i in range(len(Grid)):
         for j in range(len(Grid)-i-1):
-            if Grid[j].y > Grid[j+1].y:
+            if Grid[j].y < Grid[j+1].y:
                 temp = Grid[j+1].y
                 Grid[j+1].y = Grid[j].y
                 Grid[j].y = temp
@@ -47,32 +66,43 @@ def bubblesort(Grid):
         Grid[len(Grid)-1-i].draw()
         
         
-       
-
 def main():
-    n = 200
-    width = HEIGHT // n
     Grid = []
-    clk = pygame.time.Clock()
-    for i in range(n+1):
-        Grid.append(block(i, WIDTH, width, BLACK))
+    for i in range(N):
+        Grid.append(block(i, HEIGHT, BLACK))
+
+    print('-------------------------------------------------------------------------------------')
+    print('| Draw Vertical Bars [BLUE] and then press [SPACEBAR] to start Bubblesort Algorithm |')
+    print('-------------------------------------------------------------------------------------')
+
     
     while True:
         for event in pygame.event.get():
-            if pygame.mouse.get_pressed()[0]:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                    
+            elif pygame.mouse.get_pressed()[0]:
                 x, y = pygame.mouse.get_pos()
                 x = x // width
-                Grid[x].y = y
-                Grid[x].color = BLUE
-                Grid[x].draw()
+                if x < N:
+                    Grid[x].y = y
+                    Grid[x].color = BLUE
+                    Grid[x].draw()
+            
+            elif pygame.mouse.get_pressed()[1]:
+                x, y = pygame.mouse.get_pos()
+                x = x // width
+                if x < N:
+                    Grid[x].y = HEIGHT
+                    Grid[x].color = BLACK
+                    Grid[x].draw()
                 
-                
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     bubblesort(Grid)
                     draw_all(Grid)
         
         
-
-main()
+if __name__ == "__main__":
+    main()
         
