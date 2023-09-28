801a2c59c0d7637c73cc3e96bbbc77cab62d762e
MUKESH BHARTI <94177051+MukeshMB@users.noreply.github.com>
2023-09-29 00:42:14 +0530

    dfs.py

--- dfs.py
+++ dfs.py
@@ -1,5 +1,26 @@
 import pygame
 
+# FRAME CONSTRUCTION
+def init_display():
+	WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+	WIDTH = 9 * pygame.display.Info().current_w // 10
+	HEIGHT = 9 * pygame.display.Info().current_h // 10
+	pygame.quit()
+	WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.display.set_caption('DFS TRAVERSAL')
+
+	return WIN, WIDTH, HEIGHT
+
+
+# INIT FRAME
+WIN, WIDTH, HEIGHT = init_display()
+
+# INIT @params
+M = 50
+width = WIDTH // M
+N = HEIGHT // width
+
+# INIT Colours
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -11,16 +32,11 @@ PURPLE = (128, 0, 128)
 YELLOW = (102, 102, 0)
 MAGENTA = (102, 102, 105)
 
-WIDTH = 900
-n = 30
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption('bfs visualisation')
 
 class Node:
-	def __init__(self, row, col, width):
+	def __init__(self, row, col):
 		self.row = row
 		self.col = col
-		self.width = width
 		self.x = row * width
 		self.y = col * width
 		self.color = WHITE
@@ -66,12 +82,14 @@ class Node:
 		return self.color == ORANGE
 
 	def draw(self):
-		pygame.draw.rect(WIN, self.color, (self.x, self.y, self.width, self.width))
+		pygame.draw.rect(WIN, self.color, (self.x, self.y, width, width))
+		pygame.draw.line(WIN, (0, 0, 0), (self.x, 0), (self.x, HEIGHT))
+		pygame.draw.line(WIN, (0, 0, 0), (0, self.y), (WIDTH, self.y))
 
 	def update_neighbors(self, grid):
-		if self.col < n-1 and not grid[self.row][self.col+1].is_barrier():
+		if self.col < N-1 and not grid[self.row][self.col+1].is_barrier():
 			self.neighbors.append(grid[self.row][self.col+1])
-		if self.row < n-1 and not grid[self.row+1][self.col].is_barrier():
+		if self.row < M-1 and not grid[self.row+1][self.col].is_barrier():
 			self.neighbors.append(grid[self.row+1][self.col])
 		if self.col > 0 and not grid[self.row][self.col-1].is_barrier():
 			self.neighbors.append(grid[self.row][self.col-1])
@@ -79,23 +97,14 @@ class Node:
 			self.neighbors.append(grid[self.row-1][self.col])
 		
 
-def make_grid(width):
+def make_grid():
 	Grid = []
-	for i in range(0, n):
+	for i in range(M):
 		Grid.append([])
-		for j in range(0, n):
-			Grid[i].append(Node(i, j, width))
+		for j in range(N):
+			Grid[i].append(Node(i, j))
 
 	return Grid
-
-
-def draw_screen(Grid):
-	for list in Grid:
-		for node in list:
-			node.draw()
-			pygame.draw.line(WIN, (0, 0, 0), (node.x, 0), (node.x, WIDTH))
-			pygame.draw.line(WIN, (0, 0, 0), (0, node.y), (WIDTH, node.y))
-	pygame.display.update()
 
 
 def fill_neighbors(Grid):
@@ -105,11 +114,6 @@ def fill_neighbors(Grid):
 			node.update_neighbors(Grid)
 
 
-def pause():
-	clk = pygame.time.Clock()
-	clk.tick(60)
-
-
 def spill(Grid):
 	for list in Grid:
 		for node in list:
@@ -117,58 +121,82 @@ def spill(Grid):
 				node.reset()
 
 
-def dfs(start, node, end, Grid, vis, c, n):
-	if n and c==n:
-		n = c
-		return 0
-
-	for neig in node.neighbors:
-		if neig != start and not vis[neig.row][neig.col]:
-			if neig != end:
-				vis[neig.row][neig.col] = True
-				neig.make_visit()
-				pause()
-				draw_screen(Grid)
-				dfs(start, neig, end, Grid, vis, c+1, n)
-				neig.reset()
-				draw_screen(Grid)
-			else:
-				n = c
-				return 0
-
-	return 0
-
-
-def call_dfs(start, end, Grid):
-	par = {}
+def dfs(start, end, Grid):
 	vis = []
 	for row in Grid:
 		list = []
 		for col in row:
 			list.append(False)
 		vis.append(list)
-	n = 0
-	dfs(start, start, end, Grid, vis, 1, n)
+
+	stack = [start]
+	vis[start.row][start.col] = True
+
+	par = {}
+	Path = []
+	curr = -1
+	while len(stack):
+		node = stack[-1]
+		stack.remove(node)
+
+		if not node.is_start() and not node.is_end():
+			node.make_visited()
+			node.draw()
+		
+		if node.is_end():
+			key = end
+			n = 0
+			while key != start:
+				key = par[key]
+				n += 1
+			if n < curr or curr == -1:
+				curr = n
+				for each in Path:
+					each.make_visit()
+					each.draw()
+				
+				Path.clear()
+				key = end
+				while key != start:
+					key = par[key]
+					if not key.is_start():
+						Path.append(key)
+						key.make_path()
+						key.draw()
+
+		for each in node.neighbors:
+			if not vis[each.row][each.col]:
+				stack.append(each)
+				vis[each.row][each.col] = True
+				par[each] = node
+				if not each.is_start() and not each.is_end():
+					each.make_visit()
+					each.draw()
+		
+		pygame.display.update()
+
 
 def main():
-	width = WIDTH // n
-	Grid = make_grid(width)
+	Grid = make_grid()
+	for list in Grid:
+			for each in list:
+				each.draw()
+
+	pygame.display.update()
 
 	start = None
 	end = None
 	run = True
 
-	print('-------------------------------------------')
-	print('| press [spacebar] to start dfs traversal |')
-	print('-------------------------------------------')
+	print('--------------------------------------------------------------------------------------------------------------------')
+	print('| SELECT START NODE [GREEN], END NODE [RED], BARRIER NODE [BLACK] and then press [SPACEBAR] to start DFS Traversal |')
+	print('--------------------------------------------------------------------------------------------------------------------')
 
 	while run:
-		draw_screen(Grid)
-
+		pygame.display.update()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				run = False
-				return -1
+				pygame.quit()
 			
 			if pygame.mouse.get_pressed()[0]:
 				row, col = pygame.mouse.get_pos()
@@ -179,13 +207,16 @@ def main():
 				if not start and spot != end:
 					start = spot
 					start.make_start()
+					start.draw()
 				
 				elif not end and spot != start:
 					end = spot
 					end.make_end()
+					end.draw()
 				
 				elif spot != start and spot != end:
 					spot.make_barrier()
+					spot.draw()
 
 			if pygame.mouse.get_pressed()[2]:
 				row, col = pygame.mouse.get_pos()
@@ -197,12 +228,16 @@ def main():
 				elif spot == end:
 					end = None
 				spot.reset()
+				spot.draw()
 				
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE:
-					print('started bfs traversal')
+					print('[+] STARTED BFS TRAVERSAL [+]')
 					fill_neighbors(Grid)
-					call_dfs(start, end, Grid)	
+					dfs(start, end, Grid)	
+					print('[+] DFS ENDS  [+]')
 
-main()
+
+if __name__ == "__main__":
+	main()
 
